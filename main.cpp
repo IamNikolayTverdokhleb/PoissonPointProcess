1c4e99d20c58230a0d2fad13ec64f745d25cd4bb
Коля <kola@MacBook-Pro-Kola.local>
2019-07-01 16:23:31 +0300

    v1.1

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 int main() {
-    double lambda = 1.9; /*Выберите параметр распределения */
+    double lambda = 1.9; /*Choose parameter lambda */
     double tau = 0.001;
-    int t = 0.0; /*Выберите начальное время */
-    int T = 15.0; /*Выберите конечное время */
+    int t = 0.0; /*Choose start time */
+    int T = 15.0; /*Choose ending time */
     struct ParametrsStruct parametrsStruct = {lambda, tau, t, T};
-
+        
     probabilities prob(parametrsStruct);
     prob.runProcess();
     system("../Results/Distribution");
